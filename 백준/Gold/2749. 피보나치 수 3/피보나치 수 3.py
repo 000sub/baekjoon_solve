6b324ca5a8b6afcb53502d4bd48d645818a4d1a3
dmong <85350805+000sub@users.noreply.github.com>
2022-08-25 10:13:08 +0900

    [Gold II] Title: 피보나치 수 3, Time: 76 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Gold/2749. 피보나치 수 3/피보나치 수 3.py
@@ -0,0 +1,24 @@
+n = int(input())
+k=1_000_000
+def m_mul(a, b):
+    rst = [[0,0],[0,0]]
+
+    rst[0][0] = (a[0][0]*b[0][0] + a[0][1]*b[1][0]) % k
+    rst[0][1] = (a[0][0]*b[0][1] + a[0][1]*b[1][1]) % k
+    rst[1][0] = (a[1][0]*b[0][0] + a[1][1]*b[1][0]) % k
+    rst[1][1] = (a[1][0]*b[0][1] + a[1][1]*b[1][1]) % k
+
+    return rst
+
+def fibo(n):
+    if n==1:
+        return [[1,1],[1,0]]
+    else:
+        k = fibo(n//2)
+        if n%2==0:
+            return m_mul(k,k)
+        else:
+            return m_mul(m_mul(k, k), fibo(1))
+    
+
+print(fibo(n)[0][1])
