55fb6558bd7336d799821e5af6167bd0f0edf312
dmong <85350805+000sub@users.noreply.github.com>
2022-08-25 11:45:17 +0900

    [Gold II] Title: 짝수번째 피보나치 수의 합, Time: 80 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Gold/11443. 짝수번째 피보나치 수의 합/짝수번째 피보나치 수의 합.py
@@ -0,0 +1,28 @@
+n = int(input())
+
+x=1_000_000_007
+def m_mul(a, b):
+    rst = [[0,0],[0,0]]
+
+    rst[0][0] = (a[0][0]*b[0][0] + a[0][1]*b[1][0]) %x
+    rst[0][1] = (a[0][0]*b[0][1] + a[0][1]*b[1][1]) %x
+    rst[1][0] = (a[1][0]*b[0][0] + a[1][1]*b[1][0]) %x
+    rst[1][1] = (a[1][0]*b[0][1] + a[1][1]*b[1][1]) %x
+
+    return rst
+
+def fibo(n):
+    if n==1:
+        return [[1,1],[1,0]]
+    else:
+        k = fibo(n//2)
+        if n%2==0:
+            return m_mul(k,k) 
+        else:
+            return m_mul(m_mul(k, k), fibo(1)) 
+    
+
+n = n-1 if n&1 else n
+
+rst = (fibo(n+1)[0][1]-1) % x
+print(rst)
