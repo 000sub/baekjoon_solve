44f4f7891d66d05e12fa318ce8ee1e03ce0c4d01
dmong <dt2980_hee@naver.com>
2022-09-01 19:27:57 +0900

    [Silver I] Title: 포도주 시식, Time: 80 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.py
@@ -0,0 +1,27 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+L=[]
+for _ in range(n):
+    L.append(int(input()))
+if n==1:
+    print(L[-1])
+elif n==2:
+    print(L[0]+L[1])
+elif n==3:
+    print(sum(L) - min(L))
+else:
+    dp = [0 for _ in range(n)]
+
+    dp[0] = L[0]
+    dp[1] = L[0]+L[1]
+    dp[2] = max(dp[1],max(L[0]+L[2], L[1]+L[2]))
+
+    for i in range(3, n):
+        dp[i] = max(dp[i-2]+L[i],\
+                    dp[i-3]+L[i-1]+L[i])
+
+        dp[i] = max(dp[i-1],dp[i])
+                    
+    print(dp[-1])
