de8b059289d46f55b5b81dd03f09591b09f88360
dmong <dt2980_hee@naver.com>
2023-09-29 08:14:53 +0900

    [level 1] Title: 소수 찾기, Time: 1.80 ms, Memory: 4.48 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.cpp
@@ -0,0 +1,23 @@
+#include <string>
+#include <vector>
+#include <cstring>
+#include <iostream>
+
+using namespace std;
+bool arr[1000001];
+
+int solution(int n) {
+    int answer = 0;
+    memset(arr, true, sizeof(arr));
+    for (int i=2; i*i<=n; i++){
+        if (arr[i]){
+            for (int j=i*i; j<=n; j+=i) arr[j] = false;
+        }        
+    }
+    
+    for (int i=2; i<=n; i++){
+        if (arr[i]) answer++;
+        
+    }
+    return answer;
+}
