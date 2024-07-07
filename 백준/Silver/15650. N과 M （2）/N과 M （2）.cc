41261bda10cbeedf5a8b5db1a671e103e22903b2
dmong <dt2980_hee@naver.com>
2024-07-07 18:52:36 +0900

    [Silver III] Title: N과 M (2), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.cc
@@ -0,0 +1,34 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <iostream>
+#include <map>
+#include <cmath>
+
+using namespace std;
+
+vector<int> v;
+int n,m;
+
+void bt(int idx, int last){
+	if (idx == m) {
+		for (int i: v) cout<<i<<" ";
+		cout<<'\n';
+		return;
+	}
+	
+	for (int i=last+1; i<=n; i++) {
+		v.push_back(i);
+		bt(idx+1, i);
+		v.pop_back();
+	}
+	
+	
+}
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0); cout.tie(0);
+	cin>>n>>m;
+	bt(0, 0);
+}
