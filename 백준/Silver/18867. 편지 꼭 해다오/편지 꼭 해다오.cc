c7d3f7ef15b48cb07005920bff98c985564dbf6b
dmong <dt2980_hee@naver.com>
2024-08-03 17:38:31 +0900

    [Silver I] Title: 편지 꼭 해다오, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/18867. 편지 꼭 해다오/편지 꼭 해다오.cc
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+
+#define PII pair<int,int>
+#define PFF pair<float, float>
+
+using namespace std;
+typedef long long ll;
+
+//vector<int> v(130, 1), v2;
+//const int mod = 20200429;
+//bool visited[130];
+//
+//void f(int idx, int cnt, int sum) {
+//    if (cnt >= 5) {
+//        if (sum % mod == 20200402) {
+//            for (int i : v2) cout << i << ' ';
+//            cout << '\n';
+//        }
+//        return;
+//    }
+//    
+//    for (int i = idx ; i <= 122; i++) {
+//        if (!visited[i]) {
+//            visited[i] = true;
+//            v2.push_back(i);
+//            f(i + 1, cnt + 1, sum + v[i]);
+//            v2.pop_back();
+//            visited[i] = false;
+//        }
+//    }
+//
+//
+//}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+
+    //for (int i = 1; i <= 122; i++) {
+    //    for (int j = 0; j < 814; j++) {
+    //        v[i] = (v[i] * i) % mod;
+    //    }
+    //}
+
+    //f(32, 0, 0);
+    // ans 
+    //33 47 53 96 100 !/5`d
+    //44 62 77 107 112 ,>Mkp
+
+    cout << "!/5`d";
+
+    return 0;
+}
