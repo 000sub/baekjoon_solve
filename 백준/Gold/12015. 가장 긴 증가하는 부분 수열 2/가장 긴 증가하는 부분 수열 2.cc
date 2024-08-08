113701c13d195ccbf1f2c09126c64fc173dd0148
dmong <dt2980_hee@naver.com>
2024-08-09 02:41:21 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 2, Time: 144 ms, Memory: 13740 KB -BaekjoonHub

---
+++ 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.cc
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+
+#define PII pair<int,int>
+#define PFF pair<float, float>
+
+using namespace std;
+typedef long long ll;
+int arr[1000001], dp[1000001], minArr[1000001];
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+
+    int n;
+    cin >> n;
+    for (int i = 1; i <= n; i++) cin >> arr[i];
+    fill(minArr, minArr + n + 1, 1e9);
+    for (int i = 1; i <= n; i++) {
+
+        int idx = lower_bound(minArr, minArr + i, arr[i]) - minArr;
+        minArr[idx] = min(minArr[idx], arr[i]);
+        dp[i] = idx + 1;
+    }
+
+    int ans = 0;
+    for (int i = 1; i <= n; i++) ans = max(ans, dp[i]);
+
+    cout << ans;
+
+    return 0;
+}
