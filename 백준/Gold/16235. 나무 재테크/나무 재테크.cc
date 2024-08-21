79c5c7018495e0ddee968281859ccdbc37702428
Youngseop Shin <dt2980_hee@naver.com>
2024-08-21 23:42:29 +0900

    [Gold III] Title: 나무 재테크, Time: 0 ms, Memory: 2156 KB -BaekjoonHub

--- 백준/Gold/16235. 나무 재테크/나무 재테크.cc
+++ 백준/Gold/16235. 나무 재테크/나무 재테크.cc
@@ -4,108 +4,87 @@
 using namespace std;
 typedef long long ll;
 
-vector<int> treeMap[11][11];
-vector<int> deadTreeMap[11][11];
+vector<PII> treeMap[11][11];
 int foodMap[11][11], A[11][11];
 int N, M, K;
 int dx[] = { -1,-1,-1,0,0,1,1,1 };
 int dy[] = { 1,0,-1,1,-1,1,0,-1 };
 
-void spring() {
+void spring_summer_winter() {
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < N; j++) {
-            vector<int> tempVec;
-            vector<int>& cur = treeMap[i][j];
+            int foodFromDead = 0;
 
-            sort(cur.begin(), cur.end());
-
-            for (int age : cur) {
-                if (age <= foodMap[i][j]) {
-                    foodMap[i][j] -= age;
-                    tempVec.push_back(age + 1);
+            for (int k = treeMap[i][j].size()-1; k>=0 ; k--) {
+                int age = treeMap[i][j][k].first;
+                int cnt = treeMap[i][j][k].second;
+                int sum = age * cnt;
+                if (foodMap[i][j] >= sum) {
+                    foodMap[i][j] -= sum;
+                    treeMap[i][j][k].first++;
                 }
                 else {
-                    deadTreeMap[i][j].push_back(age);
+                    int aliveCnt = foodMap[i][j] / age;
+                    int deadCnt = cnt - aliveCnt;
+                    foodFromDead += deadCnt * (age / 2);
+                    if (aliveCnt == 0) {
+                        if (K == 1) {
+                            cout << "";
+                        }
+                        treeMap[i][j].erase(treeMap[i][j].begin() + k);
+                    }
+                    else {
+                        foodMap[i][j] -= aliveCnt * age;
+                        treeMap[i][j][k].second = aliveCnt;
+                        treeMap[i][j][k].first++;
+                    }
                 }
             }
-
-            cur = tempVec;
+            foodMap[i][j] += foodFromDead + A[i][j];
         }
     }
     
 }
 
-void summer() {
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) {
-            vector<int>& cur = deadTreeMap[i][j];
-            if (cur.empty()) continue;
-
-            for (int age : cur) {
-                foodMap[i][j] += age / 2;
-            }
-
-            cur.clear();
-        }
-    }
-}
-
 void fall() {
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < N; j++) {
-            vector<int>& cur = treeMap[i][j];
-            for (int age : cur) {
+            for (PII& p : treeMap[i][j]) {
+                int age = p.first;
+                int cnt = p.second;
                 if (age % 5 != 0) continue;
 
                 for (int d = 0; d < 8; d++) {
                     int nx = i + dx[d];
                     int ny = j + dy[d];
-
                     if (nx<0 || ny<0 || nx>N - 1 || ny>N - 1) continue;
-                    treeMap[nx][ny].push_back(1);
+                    if (treeMap[nx][ny].empty() || treeMap[nx][ny].back().first != 1)
+                        treeMap[nx][ny].push_back({ 1, cnt });
+                    else
+                        treeMap[nx][ny].back().second += cnt;
                 }
             }
         }
     }
-}
-
-void winter() {
-    for (int i = 0; i < N; i++)
-        for (int j = 0; j < N; j++)
-            foodMap[i][j] += A[i][j];
 }
 
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
     cin >> N >> M >> K;
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) {
-            cin >> A[i][j];
-        }
-        fill(foodMap[i], foodMap[i] + N, 5);
-    }
-
-    int x, y, z;
-    for (int i = 0; i < M; i++) {
+    for (int i = 0; i < N; i++) for (int j = 0; j < N; j++) cin >> A[i][j], foodMap[i][j] = 5;  
+    for (int i = 0,x,y,z; i < M; i++) {
         cin >> x >> y >> z;
-        treeMap[x-1][y-1].push_back(z);
+        treeMap[x-1][y-1].push_back({z, 1});
     }
-
-    for (int i=0; i<N; i++)
-        for (int j=0; j<N; j++)
-            sort(treeMap[i][j].begin(), treeMap[i][j].end());
-
     while (K--) {
-        spring();
-        summer();
+        spring_summer_winter();
         fall();
-        winter();
     }
     int ans = 0;
     for (int i = 0; i < N; i++)
         for (int j = 0; j < N; j++)
-            ans += treeMap[i][j].size();
+            for (PII& p : treeMap[i][j]) ans += p.second;
     cout << ans;
     return 0;
 }
