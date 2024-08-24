ad8b82a505eaa17ab04b1450d004df4a09b83ca4
Youngseop Shin <dt2980_hee@naver.com>
2024-08-24 18:15:54 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 5, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24266. 알고리즘 수업 － 알고리즘의 수행 시간 5/알고리즘 수업 － 알고리즘의 수행 시간 5.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+#define PII pair<int,int>
+#define PIII pair<int,PII>
+
+using namespace std;
+typedef long long ll;
+
+int main(void) {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	ll n;
+	cin >> n;
+	cout << n * n * n << '\n' << 3;
+	return 0;
+}
