4d884c2ede0b3870ad4b975d81335dcf04662d50
000sub <dt2980_hee@naver.com>
2024-09-10 11:07:54 +0900

    [Bronze II] Title: 시험 감독, Time: 428 ms, Memory: 5928 KB -BaekjoonHub

---
+++ 백준/Bronze/13458. 시험 감독/시험 감독.cc
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+#define PII pair<int, int>
+
+using namespace std;
+typedef long long ll;
+
+int N, B, C;
+int A[1000001];
+
+int main() {
+	ios::sync_with_stdio;
+	cin.tie(0);
+	cin >> N;
+	for (int i = 0; i < N; i++) cin >> A[i];
+	cin >> B >> C;
+
+	ll ans = N;
+	for (int i = 0; i < N; i++) {
+		A[i] -= B;
+		if (A[i] > 0) {
+			ans += A[i] / C + (A[i] % C ? 1 : 0);
+		}
+	}
+	cout << ans;
+	return 0;
+}
