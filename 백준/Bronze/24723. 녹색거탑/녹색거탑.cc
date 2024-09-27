564690d4056a6104554c8f8f296c09925bb11209
000sub <dt2980_hee@naver.com>
2024-09-27 17:32:23 +0900

    [Bronze IV] Title: 녹색거탑, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24723. 녹색거탑/녹색거탑.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+#define PII pair<int,int>
+#define PIII pair<int,PII>
+using namespace std;
+typedef long long ll;
+
+int N;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cin >> N;
+	cout << (1 << N);
+	return 0;
+}
