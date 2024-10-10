26b241a435d21362dced30b7eada3f8b6a938f04
000sub <dt2980_hee@naver.com>
2024-10-10 14:28:29 +0900

    [Gold IV] Title: 비슷한 단어, Time: 92 ms, Memory: 12772 KB -BaekjoonHub

---
+++ 백준/Gold/2179. 비슷한 단어/비슷한 단어.cc
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int N, maxCnt = 0;
+string str;
+vector<string> v;
+set<string> used;
+map<string, vector<int>> MAP;
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	cin >> N;
+	for (int i = 0; i < N; i++) {
+		cin >> str;
+		v.push_back(str);
+		if (used.count(str)) continue;
+		used.insert(str);
+
+		string temp = "";
+		
+
+		for (int j = 0; j < str.size(); j++) {
+			temp += str[j];
+			MAP[temp].push_back(i);
+			if (MAP[temp].size() >= 2) maxCnt = max(maxCnt, (int)temp.size());
+		}
+	}
+
+	int minPrefixIdx = 1e9;
+	string minPrefix;
+	for (auto& p : MAP) {
+		if (p.second.size() >= 2 && p.first.size() == maxCnt) {
+			if (minPrefixIdx > p.second[0]) {
+				minPrefixIdx = p.second[0];
+				minPrefix = p.first;
+			}
+		}
+	}
+	cout << v[MAP[minPrefix][0]] << '\n' << v[MAP[minPrefix][1]];
+
+	return 0;
+}
