fa5c368fbc4f7b444ba86ec76cd75208e3000564
000sub <dt2980_hee@naver.com>
2024-10-16 01:26:04 +0900

    [Silver V] Title: 돌 게임, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/9655. 돌 게임/돌 게임.cc
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+#define PII pair<int,int>
+using namespace std;
+typedef long long ll;
+
+int N;
+
+int main()
+{
+    ios::sync_with_stdio(0), cin.tie(0);
+
+    cin >> N;
+    N % 2 ? cout << "SK" : cout << "CY";
+    
+    return 0;
+}
